ce70930888920e0eab4230dabb059413ec029fe4
ivan farid <ivan99edlp@gmail.com>
2023-07-25 19:28:45 -0300

    routes added

--- src/modules/courses/courses.service.ts
+++ src/modules/courses/courses.service.ts
@@ -1,3 +1,4 @@
+import { IEditCourseDTO } from './dtos/editCourse.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Course } from './schemas/course.schema';
@@ -15,5 +16,28 @@ export class CoursesService {
 
   async findAll(): Promise<Course[]> {
     return this.courseModel.find().exec();
+  }
+
+  async getCourseById(id: string) {
+    try {
+      const course = await this.courseModel.findById(id).exec();
+      return course
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async findByIdAndUpdate(id: string, values: IEditCourseDTO): Promise<Course> {
+    try {
+      const course = await this.courseModel.findByIdAndUpdate(id, values, {new:true});
+      console.log(course, 'ACTUALIZADO');
+      return course;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async findByIdAndDelete(id: string) {
+    return await this.courseModel.findByIdAndDelete(id)
   }
 }
